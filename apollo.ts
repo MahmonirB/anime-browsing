a1b844ffd8488955f50eff49ee10526b285b85f9
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2023-01-15 22:49:57 +0100

    feat: initializing graphql

---
+++ apollo.ts
@@ -0,0 +1,7 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { API_URL } from "./config/constants";
+
+export const client = new ApolloClient({
+  uri: API_URL,
+  cache: new InMemoryCache(),
+});
