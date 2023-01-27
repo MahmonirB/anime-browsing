25c3b4011a0e2e0ca63d005eb9f59db1f5457ee8
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2023-01-27 23:14:16 +0100

    fix: improve rules into eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,24 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:prettier/recommended"
-    ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "react/react-in-jsx-scope": "off"
-    }
-}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  files: ["*.ts", "*.tsx"],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'no-undef': 'off',
+  },
+};
