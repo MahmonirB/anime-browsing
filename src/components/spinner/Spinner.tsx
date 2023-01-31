4826f55c3406901e5f5e83a8fee5251c9138e144
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2023-02-01 00:00:12 +0100

    fontawsome

--- src/components/spinner/Spinner.tsx
+++ src/components/spinner/Spinner.tsx
@@ -1,8 +1,9 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
 function Spinner() {
     return (
-        <></>
+        <FontAwesomeIcon icon="fa-solid fa-spinner" />
     );
 }
 
