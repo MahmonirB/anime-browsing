351338b53aead41f0ad86f57ca7e7df42aea72a5
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2023-02-05 23:57:45 +0100

    fix: improve classnames

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -2,37 +2,44 @@ import React from 'react';
 
 function Pagination({ hasNextPage, total, currentPage, onChangePage }: any) {
     console.log(currentPage)
+    const commonClass = 'page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 hover:bg-gray-200 focus:shadow-none';
     return (
         <div className="flex justify-center">
             <nav aria-label="Page navigation">
                 <ul className="flex list-style-none">
                     <li className='page-item'>
                         <button
-                            className={`page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none ${currentPage - 1 === 0 ? 'text-gray-400 bg-transparent' : ''}`}
-                            aria-disabled={currentPage - 1 === 0 ? true : false}
+                            className={`${commonClass} text-gray-800 hover:text-gray-800 ${currentPage - 1 === 0 ? 'text-gray-400 bg-transparent hover:bg-transparent' : ''}`}
+                            disabled={currentPage - 1 === 0}
                             onClick={() => onChangePage(currentPage - 1)}>
                             Previous
                         </button>
                     </li>
-                    {currentPage - 1 > 0 && <li className="page-item"><button
-                        className="page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
-                        onClick={() => onChangePage(currentPage - 1)}>{currentPage - 1}</button></li>}
-                    <li className="page-item active"><button
-                        className="page-link relative block py-1.5 px-3 rounded border-0 bg-blue-600 outline-none transition-all duration-300 text-white hover:text-white hover:bg-blue-600 shadow-md focus:shadow-md"
-                        onClick={() => onChangePage(currentPage)}>
-                        {currentPage}
-                    </button>
+                    {currentPage - 1 > 0 && <li className="page-item">
+                        <button
+                            className={`${commonClass} text-gray-800 hover:text-gray-800 `}
+                            onClick={() => onChangePage(currentPage - 1)}>
+                            {currentPage - 1}
+                        </button>
+                    </li>}
+                    <li className="page-item active">
+                        <button
+                            className={`${commonClass} bg-blue-600 text-white hover:text-white hover:bg-blue-600 shadow-md focus:shadow-md`}
+                            onClick={() => onChangePage(currentPage)}>
+                            {currentPage}
+                        </button>
                     </li>
-                    <li className="page-item"><button
-                        className="page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
-                        onClick={() => onChangePage(currentPage + 1)}>
-                        {currentPage + 1}
-                    </button>
+                    <li className="page-item">
+                        <button
+                            className={`${commonClass} text-gray-800 hover:text-gray-800 `}
+                            onClick={() => onChangePage(currentPage + 1)}>
+                            {currentPage + 1}
+                        </button>
                     </li>
                     <li className='page-item'>
                         <button
-                            className={`page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none ${hasNextPage ? '' : 'text-gray-600'}`}
-                            aria-disabled={hasNextPage ? false : true}
+                            className={`${commonClass} text-gray-800 hover:text-gray-800 ${hasNextPage ? '' : 'text-gray-600'}`}
+                            disabled={!hasNextPage}
                             onClick={() => hasNextPage ? onChangePage(currentPage + 1) : null}>
                             Next
                         </button>
