213700cc9f4ee0f012ca0e9f41f1c5ed5af7eb8d
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2023-02-09 22:28:20 +0100

    feat: add vite-plugin-pwa/ add config to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'AnimeSuche',
+        short_name: 'Anime',
+        theme_color: '#fff',
+        start_url: '/',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 });
