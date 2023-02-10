354a488150225afc40c4ef439750fbcc55234bf2
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2023-02-10 21:23:15 +0100

    improve vite pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -16,12 +16,12 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: '/public/favIcons/pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: 'pwa-512x512.png',
+            src: '/public/favIcons/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
