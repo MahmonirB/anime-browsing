4134f6e4b54f8cef43a4707d271c626221a386f2
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2023-02-10 21:01:36 +0100

    feat: change vite register type

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: 'prompt',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
       manifest: {
         name: 'AnimeSuche',
