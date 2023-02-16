f3bd4db64befc0f3f596f555a3916c9b4f9faf78
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2023-02-16 22:16:37 +0100

    fix: improve header classes

--- src/components/navbar/Header.tsx
+++ src/components/navbar/Header.tsx
@@ -8,6 +8,8 @@ function Header() {
 
   const toggleNavbar = () => setShowNavbar(prev => !prev);
 
+  const linkClasses = "p-1 lg:px-4 md:mx-2 text-gray-600 dark:text-gray-300 rounded hover:bg-gray-200 hover:text-gray-700 transition-colors duration-300";
+
   return (
     <div className="header-2">
       <nav className="bg-white dark:bg-stone-800  py-5 md:py-4">
@@ -25,31 +27,19 @@ function Header() {
           </div>
 
           <div
-            className={`md:flex items-center flex-col md:flex-row md:ml-auto mt-2 p-2 md:mt-0 w-full bg-white dark:bg-stone-800 dark:border-b absolute md:relative md:w-1/2 transition duration-900 ${showNavbar ? 'flex' : 'hidden'
-              }`}
+            className={`md:flex items-center flex-col md:flex-row md:ml-auto mt-2 p-2 md:mt-0 w-full bg-white dark:bg-stone-800 dark:border-b absolute md:relative md:w-1/2 transition duration-900 ${showNavbar ? 'flex' : 'hidden'}`}
             id="navbar-collapse"
           >
-            <a
-              href="#"
-              className="p-1 lg:px-4 md:mx-2 text-gray-600 dark:text-gray-300 rounded hover:bg-gray-200 hover:text-gray-700 transition-colors duration-300"
-            >
+            <a href="#" className={linkClasses}>
               About
             </a>
-            <a
-              href="#"
-              className="p-1 lg:px-4 md:mx-2 text-gray-600  dark:text-gray-300 rounded hover:bg-gray-200 hover:text-gray-700 transition-colors duration-300"
-            >
+            <a href="#" className={linkClasses}>
               Genres
             </a>
-            <Link to="/media"
-              className="p-1 lg:px-4 md:mx-2 text-gray-600 dark:text-gray-300 rounded hover:bg-gray-200 hover:text-gray-700 transition-colors duration-300"
-            >
+            <Link to="/media" className={linkClasses}>
               Media
             </Link>
-            <a
-              href="#"
-              className="p-1 lg:px-4 md:mx-2 text-gray-600 dark:text-gray-300 rounded hover:bg-gray-200 hover:text-gray-700 transition-colors duration-300"
-            >
+            <a href="#" className={linkClasses}>
               Contact
             </a>
             <a href="#" className="p-2 lg:px-4 md:mx-2 text-white dark:text-gray-300 rounded bg-indigo-900">
