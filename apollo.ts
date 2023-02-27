60d37a4bb538f15a452bbdf30c541c3d8de2f56c
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2023-02-28 00:07:00 +0100

    feat: add apollo link to get rest api

--- apollo.ts
+++ apollo.ts
@@ -1,7 +1,13 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 import { API_URL } from './config/constants';
+import { RestLink } from 'apollo-link-rest';
 
 export const client = new ApolloClient({
   uri: API_URL,
   cache: new InMemoryCache(),
 });
+
+export const restLink = new RestLink({
+  uri: 'http://localhost:3001',
+});
+
